cb85aba8166cd7ad52ba77fcc69285294a9b2335
CPNSPath <3082240019_tito@pknstan.ac.id>
2026-03-16 16:02:37 +0700

    update

--- src/lib/packages.js
+++ src/lib/packages.js
@@ -1,44 +1,246 @@
 export const satuanTO = [
 
-{
-slug:"to-1",
-title:"Try Out 1",
-price:29000,
-soal:110,
-durasi:"100 menit"
-},
-
-{
-slug:"to-2",
-title:"Try Out 2",
-price:29000,
-soal:110,
-durasi:"100 menit"
-},
-
-{
-slug:"to-3",
-title:"Try Out 3",
-price:29000,
-soal:110,
-durasi:"100 menit"
-}
+{ slug:"to-2", title:"Try Out 2", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-3", title:"Try Out 3", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-4", title:"Try Out 4", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-5", title:"Try Out 5", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-6", title:"Try Out 6", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-7", title:"Try Out 7", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-8", title:"Try Out 8", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-9", title:"Try Out 9", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-10", title:"Try Out 10", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-11", title:"Try Out 11", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-12", title:"Try Out 12", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-13", title:"Try Out 13", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-14", title:"Try Out 14", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-15", title:"Try Out 15", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-16", title:"Try Out 16", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-17", title:"Try Out 17", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-18", title:"Try Out 18", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-19", title:"Try Out 19", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-20", title:"Try Out 20", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-21", title:"Try Out 21", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-22", title:"Try Out 22", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-23", title:"Try Out 23", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-24", title:"Try Out 24", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-25", title:"Try Out 25", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-26", title:"Try Out 26", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-27", title:"Try Out 27", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-28", title:"Try Out 28", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-29", title:"Try Out 29", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-30", title:"Try Out 30", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-31", title:"Try Out 31", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-32", title:"Try Out 32", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-33", title:"Try Out 33", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-34", title:"Try Out 34", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-35", title:"Try Out 35", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-36", title:"Try Out 36", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-37", title:"Try Out 37", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-38", title:"Try Out 38", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-39", title:"Try Out 39", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-40", title:"Try Out 40", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-41", title:"Try Out 41", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-42", title:"Try Out 42", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-43", title:"Try Out 43", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-44", title:"Try Out 44", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-45", title:"Try Out 45", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-46", title:"Try Out 46", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-47", title:"Try Out 47", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-48", title:"Try Out 48", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-49", title:"Try Out 49", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-50", title:"Try Out 50", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-51", title:"Try Out 51", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-52", title:"Try Out 52", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-53", title:"Try Out 53", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-54", title:"Try Out 54", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-55", title:"Try Out 55", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-56", title:"Try Out 56", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-57", title:"Try Out 57", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-58", title:"Try Out 58", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-59", title:"Try Out 59", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-60", title:"Try Out 60", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-61", title:"Try Out 61", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-62", title:"Try Out 62", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-63", title:"Try Out 63", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-64", title:"Try Out 64", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-65", title:"Try Out 65", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-66", title:"Try Out 66", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-67", title:"Try Out 67", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-68", title:"Try Out 68", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-69", title:"Try Out 69", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-70", title:"Try Out 70", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-71", title:"Try Out 71", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-72", title:"Try Out 72", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-73", title:"Try Out 73", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-74", title:"Try Out 74", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-75", title:"Try Out 75", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-76", title:"Try Out 76", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-77", title:"Try Out 77", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-78", title:"Try Out 78", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-79", title:"Try Out 79", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-80", title:"Try Out 80", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-81", title:"Try Out 81", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-82", title:"Try Out 82", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-83", title:"Try Out 83", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-84", title:"Try Out 84", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-85", title:"Try Out 85", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-86", title:"Try Out 86", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-87", title:"Try Out 87", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-88", title:"Try Out 88", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-89", title:"Try Out 89", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-90", title:"Try Out 90", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-91", title:"Try Out 91", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-92", title:"Try Out 92", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-93", title:"Try Out 93", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-94", title:"Try Out 94", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-95", title:"Try Out 95", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-96", title:"Try Out 96", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-97", title:"Try Out 97", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-98", title:"Try Out 98", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-99", title:"Try Out 99", price:10000, soal:110, durasi:"100 menit" },
+{ slug:"to-100", title:"Try Out 100", price:10000, soal:110, durasi:"100 menit" }
 
 ]
 
 export const bundling5 = [
 
 {
-slug:"paket-hemat",
-title:"Bundling Hemat",
-price:99000,
+slug:"paket-1",
+title:"Bundling 1",
+price:40000,
 jumlahTO:5
 },
 
 {
-slug:"paket-premium",
-title:"Bundling Premium",
-price:119000,
+slug:"paket-2",
+title:"Bundling 2",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-3",
+title:"Bundling 3",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-4",
+title:"Bundling 4",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-5",
+title:"Bundling 5",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-6",
+title:"Bundling 6",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-7",
+title:"Bundling 7",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-8",
+title:"Bundling 8",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-9",
+title:"Bundling 9",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-10",
+title:"Bundling 10",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-11",
+title:"Bundling 11",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-12",
+title:"Bundling 12",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-13",
+title:"Bundling 13",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-14",
+title:"Bundling 14",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-15",
+title:"Bundling 15",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-16",
+title:"Bundling 16",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-17",
+title:"Bundling 17",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-18",
+title:"Bundling 18",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-19",
+title:"Bundling 19",
+price:40000,
+jumlahTO:5
+},
+
+{
+slug:"paket-20",
+title:"Bundling 20",
+price:40000,
 jumlahTO:5
 }
 
@@ -47,16 +249,72 @@ jumlahTO:5
 export const bundling10 = [
 
 {
-slug:"paket-super",
-title:"Bundling Super",
-price:179000,
+slug:"paket-1",
+title:"Bundling 1",
+price:80000,
 jumlahTO:10
 },
 
 {
-slug:"paket-ultimate",
-title:"Bundling Ultimate",
-price:199000,
+slug:"paket-2",
+title:"Bundling 2",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-3",
+title:"Bundling 3",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-4",
+title:"Bundling 4",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-5",
+title:"Bundling 5",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-6",
+title:"Bundling 6",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-7",
+title:"Bundling 7",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-8",
+title:"Bundling 8",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-9",
+title:"Bundling 9",
+price:80000,
+jumlahTO:10
+},
+
+{
+slug:"paket-10",
+title:"Bundling 10",
+price:80000,
 jumlahTO:10
 }
 
