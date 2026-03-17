9375937a248021811ce6fb04fe33a92d2339399c
CPNSPath <3082240019_tito@pknstan.ac.id>
2026-03-17 15:53:19 +0700

    final fix case sensitive

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { supabase } from "@/lib/supabase"
 import { getMyPackages } from "@/lib/myPackages"
-import { getMyResults } from "@/lib/myResults"
+import { getMyResults } from "@/lib/myresults"
 
 export default function Dashboard(){
 
